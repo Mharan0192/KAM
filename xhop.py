8ecee7b3b0d7f06f693e599249fedcdcb638b4ab
Mharan0192 <32940532+Mharan0192@users.noreply.github.com>
2017-10-27 14:37:35 -0500

    updated code

--- xhop.py
+++ xhop.py
@@ -1,1 +1,11 @@
-import turtle 
+def xhop(p):
+    d = {}
+    for i in range(0,p):
+        if i == 0:
+            x = 0
+            y = 0
+            d = dict({x,y})
+            plt.scatter(x, y)
+        else:
+            if i % 8 == 1:
+                for f in range(0,i)
